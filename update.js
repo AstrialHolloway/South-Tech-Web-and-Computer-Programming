c28e6c36119846d5a09785583585f0399e75ef79
Astrial <mlsgamer09@gmail.com>
2026-09-13 22:47:01 -0500

    I just want a QOL thing D:

--- update.js
+++ update.js
@@ -1,8 +1,8 @@
 const repoOwner = 'AstrialHolloway';
 const repoName = 'South-Tech-Web-and-Computer-Programming';
 
-// FIX: Make sure this exact structure is used with backticks (`)
-fetch(`https://github.com/${repoOwner}/${repoName}`)
+// FIX: Ensure it says ://github.com
+fetch(`https://://github.com${repoOwner}/${repoName}`)
   .then(response => {
     if (!response.ok) {
       throw new Error(`GitHub API returned status: ${response.status}`);
